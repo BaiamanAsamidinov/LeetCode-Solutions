1fe96507183eb9102c98a9ea41c7ac4590a4ec18
BaiamanAsamidinov <52200571+BaiamanAsamidinov@users.noreply.github.com>
2025-09-29 00:28:35 +0200

    Time: 7 ms (99.57%), Space: 45 MB (67.53%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
@@ -1,30 +1,16 @@
 class Solution {
     public int largestPerimeter(int[] nums) {
-
-        int sum = 0;
         Arrays.sort(nums);
-
-        for(int i = nums.length - 1; i >= 0; i--){
-            for(int j = nums.length - 2; j >= 0; j--){
-                for(int k = nums.length - 3; k >= 0; k--){ // 10 2 1
-                    
-                    if(nums[i] >= (nums[j] + nums[k])) {
-                        i--;
-                        break;
-                    }
-                    System.out.println(nums[i] + " " +  nums[j] + " " + nums[k]);
-
-                    if(i != j && j != k && i != k && nums[i] > 0 && nums[j] > 0 && nums[k] > 0 && nums[i] < (nums[j] + nums[k]) && nums[j] < (nums[i] + nums[k]) && nums[k] < (nums[i] + nums[j])) {
-                        sum = nums[i] + nums[j] + nums[k];
-
-                        System.out.println(nums[i] + " " +  nums[j] + " " + nums[k] + "Answer");
-                        return sum;
-                    }
-
-                }
+        int n = nums.length;
+        
+        for (int i = n - 1; i >= 2; i--) {
+            int a = nums[i - 2];
+            int b = nums[i - 1];
+            int c = nums[i];
+            if (a + b > c) {
+                return a + b + c;
             }
         }
-
-        return sum;
+        return 0;
     }
 }
